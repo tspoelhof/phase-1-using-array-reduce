d2f723e781aa5ec7bfe6e32ceb95fd3575d786bb
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-23 21:01:33 -0400

    reduce

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(function(totalBatteries, batchCount) {
+    return totalBatteries + batchCount; }, 0);
+
+    //uses same layout as incomingStudents.reduce(function (showcases, student)
